af34184ecd36c30d0c17c0d2cf61088e5d9cb55f
Kevin Picado <kvap224@gmail.com>
2023-05-26 14:39:33 -0600

    updates

--- db/connection.go
+++ db/connection.go
@@ -2,15 +2,23 @@ package db
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
-var DSN = "host=localhost user=kevin password=kevin dbname=gorm port=8080"
 
 func DBConnection() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DSN := os.Getenv("DB_KEY")
+
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
